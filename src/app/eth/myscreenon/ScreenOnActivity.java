ba20973ed9bf47ed9d624c5f3e6c32d2e7523a22
foxpro87 <sbwoo@wizsolution.net>
2013-06-13 10:27:09 +0900

    Apply System Settings Update
    Settings.System.putInt(getContentResolver(),
    Settings.System.SCREEN_OFF_TIMEOUT, -1);

--- src/app/eth/myscreenon/ScreenOnActivity.java
+++ src/app/eth/myscreenon/ScreenOnActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.provider.Settings;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -12,6 +13,7 @@ public class ScreenOnActivity extends Activity {
 
 	PowerManager mPM;
 	PowerManager.WakeLock mWakeLock;
+    private static final int DELAY = 3000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +26,30 @@ public class ScreenOnActivity extends Activity {
 		setContentView(R.layout.activity_screen_on);
 		mPM = (PowerManager) getSystemService(Context.POWER_SERVICE);
 		mWakeLock = mPM.newWakeLock(PowerManager.FULL_WAKE_LOCK,"MY WAKE LOCK TAG");
-        mWakeLock.acquire();
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mWakeLock.acquire();
                 finish();
             }
         });
+
+        //Settings.System.putInt(resolver, SCREEN_OFF_TIMEOUT, DELAY);
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // screen off after 15 seconds
+                //Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, 15000);
+
+                // Never sleep
+                Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, -1);
+                finish();
+            }
+        });
+
 	}
 
 	public void MAIN_ONCLICK(View v) {
@@ -42,6 +59,7 @@ public class ScreenOnActivity extends Activity {
 			break;
 		case R.id.button2:
 			if (mWakeLock.isHeld()) {
+                mWakeLock.acquire();
 				mWakeLock.release();
                 finish();
 			}
